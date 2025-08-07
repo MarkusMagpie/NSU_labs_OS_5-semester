11a75661558a08c862e139eed0a3da9273aab32d
MarkusMagpie <m.sorokin3@g.nsu.ru>
2025-08-07 21:18:34 +0700

    cosmetic changes to lab1_3

--- threads/labC/lab1_3/b/bad_detached_variable.c
+++ threads/labC/lab1_3/b/bad_detached_variable.c
@@ -8,6 +8,8 @@
 #include <stdlib.h>
 
 void* func(void* arg) {
+    /* так как func() - detached поток, то переданный ему аргумент arg, лежащий на стеке потока main(), 
+    может быть стерт вместе со стеком потока main(), а значит буду выводить мусор*/ 
     printf("[func] number = %d, message = %s\n", ((int*)arg)[0], ((char**)arg)[1]);
     return NULL;
 }
@@ -17,7 +19,7 @@ int main() {
 
     int number = 1;
     char message[] = "hello everynyan";
-    void* arg[2] = { &number, message };
+    void* arg[2] = { &number, message }; // локальная переменная arg, лежит на стеке потока main()
 
     pthread_attr_t attr;
     pthread_attr_init(&attr);
@@ -25,7 +27,7 @@ int main() {
 
     printf("[main] создание потоков в detached состоянии\n");
 
-    // передаём адрес массива arg, расположенного в стеке
+    // передаю адрес arg detached потоку func()
     int err = pthread_create(&tid, &attr, func, arg);
     if (err) {
         printf("[main] pthread_create() failed: %s\n", strerror(err));

e5ec8788bc7f2b947d82443ecaf698a6f9d1e42f
MarkusMagpie <m.sorokin3@g.nsu.ru>
2025-08-09 15:28:50 +0700

    cosmetic changes to lab1_7

--- threads/labA/lab1_7/uthread.c
+++ threads/labA/lab1_7/uthread.c
@@ -1,6 +1,6 @@
 #include "uthread.h"
 
-static ucontext_t ucontexts[UTHREAD_MAX];
+static ucontext_t ucontexts[UTHREAD_MAX]; // массив контекстов юзер потоков
 static char ustacks[UTHREAD_MAX][UTHREAD_STACK_SIZE];
 static void *(*ufuncs[UTHREAD_MAX])(void *);
 static void *uargs[UTHREAD_MAX];
@@ -8,35 +8,37 @@ static void *uargs[UTHREAD_MAX];
 static int ucount = 0;
 static int current = -1;
 
-// для запуска потоковой функции
+// точка входа в каждый юзер поток после переключения контекста
 static void uthread_wrapper(void) {
     int id = current;
     void *ret = ufuncs[id](uargs[id]);
     printf("[uthread_wrapper] поток %d завершил работу.\n", id);
 
     uthread_yield();
-    // опять вернулись сюда после передачи управления следующему потоку
+    
+    // попал сюда после передачи управления следующему потоку -> этот поток завершается
     exit(0);
 }
 
-// создаем юзер поток: подготавливаем контекс, "стек", инкременируем счетчик юзер потоков ucount
+// создание юзер потока: контекст, "стек", инкременируем счетчик юзер потоков ucount
 int uthread_create(uthread_t *thread, void *(*start_routine)(void *), void *arg) {
     if (ucount >= UTHREAD_MAX) {
         return -1;
     }
     
-    // создаем контекст
+    // инициализирую контекст текущего потока
     int id = ucount++;
     if (getcontext(&ucontexts[id]) == -1) {
         return -1;
     }
 
     ucontexts[id].uc_link = NULL; // когда контекст завершится, никакой другой не запустится 
-    ucontexts[id].uc_stack.ss_sp = ustacks[id]; // стек заранее выделен в статическом массиве
-    ucontexts[id].uc_stack.ss_size = UTHREAD_STACK_SIZE;
+    ucontexts[id].uc_stack.ss_sp = ustacks[id]; // адрес начала стека - сам стек заранее выделен в массиве ustacks
+    ucontexts[id].uc_stack.ss_size = UTHREAD_STACK_SIZE; // размер стека
     
     ufuncs[id] = start_routine;
     uargs[id] = arg;
+
     makecontext(&ucontexts[id], uthread_wrapper, 0); 
 
     *thread = id;
@@ -52,12 +54,7 @@ void uthread_yield(void) {
     int prev = current;
     current = (current + 1) % ucount;
 
-    if (prev == -1) {
-        // первый запуск юзер потока
-        setcontext(&ucontexts[current]);
-    } else {
-        swapcontext(&ucontexts[prev], &ucontexts[current]);
-    }
+    swapcontext(&ucontexts[prev], &ucontexts[current]);
 }
 
 // запуск первого юзер потока

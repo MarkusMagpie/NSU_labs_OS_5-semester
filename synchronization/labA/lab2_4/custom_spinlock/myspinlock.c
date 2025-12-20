b2ba77cb747722372ef75f2793b99e0b9ee44ab6
MarkusMagpie <m.sorokin3@g.nsu.ru>
2025-12-20 22:15:41 +0700

    lab2_4 доп

--- synchronization/labA/lab2_4/custom_spinlock/myspinlock.c
+++ synchronization/labA/lab2_4/custom_spinlock/myspinlock.c
@@ -13,6 +13,18 @@ void custom_spinlock_init(custom_spinlock_t *s) {
     s->cnt = 0; 
 }
 
+// 1 вариант
+// void custom_spinlock_init(custom_spinlock_t *s) {
+//     atomic_init(&s->lock, 0);
+//     atomic_init(&s->owner, 0);
+//     atomic_init(&s->cnt, 0);
+// }
+
+// 2 вариант
+// void custom_spinlock_init(custom_spinlock_t *s) {
+//     atomic_init(&s->lock, 0);
+// }
+
 void custom_spinlock_lock(custom_spinlock_t *s) {
 	while (1) {
 		int expected = 0;
@@ -24,6 +36,16 @@ void custom_spinlock_lock(custom_spinlock_t *s) {
         // продолжаю проверку в цикле пока лок не станет доступен
 	}
 }
+
+// 2 вариант
+// void custom_spinlock_lock(custom_spinlock_t *s) {
+//     while (1) {
+//         int expected = 0;
+//         if (atomic_compare_exchange_strong(&s->lock, &expected, 1)) {
+//             return;
+//         }
+//     }
+// }
 
 void custom_spinlock_unlock(custom_spinlock_t *s) {
     // старый вариант
@@ -45,3 +67,8 @@ void custom_spinlock_unlock(custom_spinlock_t *s) {
         printf("custom_spinlock_unlock(): attempt to unlock spinlock not owned by current thread\n");
     }
 }
+
+// 2 вариант
+// void custom_spinlock_unlock(custom_spinlock_t *s) {
+//     atomic_store(&s->lock, 0);
+// }
--- synchronization/labA/lab2_4/custom_spinlock/myspinlock.h
+++ synchronization/labA/lab2_4/custom_spinlock/myspinlock.h
@@ -7,6 +7,18 @@ typedef struct {
     int cnt; // счётчик захватов лока
 } custom_spinlock_t;
 
+/* доп: race condition в custom_spinlock_unlock()?
+    1 - сделать операции атомарными. Для этого owner и cnt должны стать таковыми
+    2 - убрать реентабельность в myspinlock.c
+*/
+
+// 1 вариант
+// typedef struct {
+//     atomic_int lock; // статусы лока: 0 - свободен, 1 - занят
+//     atomic_int owner; // tid того кто владелец
+//     atomic_int cnt; // счётчик захватов лока
+// } custom_spinlock_t;
+
 void custom_spinlock_init(custom_spinlock_t *s);
 void custom_spinlock_lock(custom_spinlock_t *s);
 void custom_spinlock_unlock(custom_spinlock_t *s);
